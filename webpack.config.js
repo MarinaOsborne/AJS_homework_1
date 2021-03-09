7a210074b6cefd3d843d8263e6f874894e6c85a1
Marina Osborne <65596875+MarinaOsborne@users.noreply.github.com>
2021-03-09 22:14:23 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
+const CopyWebpackPlugin = require('copy-webpack-plugin'); 
 module.exports = {
+  entry: './src/index.js',
   output: {
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
@@ -17,13 +18,6 @@ module.exports = {
         },
       },
       {
-        test: /\.(png|jpe?g|ico)$/i,
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
-        },
-      },
-      {
         test: /\.html$/,
         use: [
           {
@@ -31,15 +25,26 @@ module.exports = {
           },
         ],
       },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader, 'css-loader',
-        ],
-      },
+        {
+              test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+              type: 'asset/resource',
+            },
+        {
+              test: /\.css$/,
+              use: [
+              MiniCssExtractPlugin.loader, 'css-loader',
+              ],
+        },
+            
     ],
   },
   plugins: [
+    new CopyWebpackPlugin({
+			patterns: [{
+				from: path.resolve(__dirname, 'src/img'), to: path.resolve(__dirname, 'dist/img'),
+        from: path.resolve(__dirname, 'src/favicon.ico'), to: path.resolve(__dirname, 'dist'),
+			}]
+		}),
     new HtmlWebPackPlugin({
       template: './src/index.html',
       filename: './index.html',
